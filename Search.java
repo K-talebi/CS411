db0c7aa79efddecc81a69e96591fa4fa8036aa65
K-talebi <K-talebi@users.noreply.github.com>
2016-02-24 21:15:19 -0400

    Update Search.java

--- Search.java
+++ Search.java
@@ -1,10 +1,10 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class Search {
-	private int numberOfNodes;
+	private int numberOfNodes = 0;
 	
 	public Search() {
-		numberOfNodes = 0;
 	}
 
 	public void resetNodeCounter(){
@@ -19,27 +19,26 @@ public class Search {
 	
 	public Puzzle solve(Puzzle start, Puzzle goal){
 		ArrayList<Puzzle> explored = new ArrayList<>();
-		ArrayList<Puzzle> frontier = new ArrayList<>();
+		PriorityQueue<Puzzle> frontier = new PriorityQueue<>();
 		resetNodeCounter();
 		frontier.add(start);
 		numberOfNodes = 1;
 		
+		//System.out.println(start);
 		while(frontier.size()>0){
-			Puzzle progress  = getLowestCost(frontier);
-			frontier.remove(progress);
+			Puzzle progress  = frontier.poll();
+//			System.out.println("PROGRESS ON PUZZLE");
+//			System.out.println(progress);
 			
 			//check if solved
 			if(progress.equals(goal)){
 				return progress;
 			}
-			
-			if (frontier.indexOf(progress) > -1 || explored.indexOf(progress) > -1) {
-                continue;
-            }
             
 			//If not, get children of current node, add to frontier
 			//add current to explored set, run again on frontier.
 			ArrayList<Puzzle> childPuzzles = getChildren(progress);
+//			System.out.println(childPuzzles);
 			for(int i = 0; i < childPuzzles.size(); i++){
 				if((frontier.contains(childPuzzles.get(i))) || ((explored.contains(childPuzzles.get(i))))){
 					//System.out.println("Already had");
@@ -52,35 +51,45 @@ public class Search {
 			}
 			explored.add(progress);	
 		}
+		System.out.println();
 		return null;
 	}
 	
 	//find possible directions to go
 	public ArrayList<Puzzle> getChildren(Puzzle parent){
 		ArrayList<Puzzle> childList = new ArrayList<Puzzle>();
-		Puzzle child;
+		//Puzzle child;
 		if(parent.canMoveUp()){
-			child = new Puzzle(parent);
+			Puzzle child = new Puzzle(parent);
 			child.moveUp();
 			childList.add(child);
+//			System.out.println("MOVED UP");
+//			System.out.println(child);
 		}
 		
 		if(parent.canMoveDown()){
-			child = new Puzzle(parent);
+			Puzzle child = new Puzzle(parent);
 			child.moveDown();
 			childList.add(child);
+//			System.out.println("Moved down");
+//			System.out.println(child);
 		}
 
 		if(parent.canMoveLeft()){
-			child = new Puzzle(parent);
+			Puzzle child = new Puzzle(parent);
 			child.moveLeft();
 			childList.add(child);
+//			System.out.println("MOVED LEFT");
+			//System.out.println(child);
 		}
 
 		if(parent.canMoveRight()){
-			child = new Puzzle(parent);
+			
+			Puzzle child = new Puzzle(parent);
 			child.moveRight();
 			childList.add(child);
+//			System.out.println("Move Right");
+			//System.out.println(child);
 		}
 		
 		return childList;
@@ -100,8 +109,8 @@ public class Search {
 				bestChoice = checkingPuzzle;
 			}
 		}
-		System.out.println("SEARCH CHOSE THIS!");
-		System.out.println(bestChoice);
+		//System.out.println("Best Choicee");
+		//System.out.println(bestChoice);
 		return bestChoice;
 	}
 }
