ed7783d35c64895e3d86109bdfe5f0513a73f185
K-talebi <K-talebi@users.noreply.github.com>
2016-03-09 00:47:44 -0400

    Update Main.java

--- AS2/Main.java
+++ AS2/Main.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class Main {
 	public static void main(String[] args) {
 		Board board = new Board();
@@ -7,10 +9,13 @@ public class Main {
 		
 		final int NUMBER_OF_PUZZLES = 1000;
 		
-		int counter = 0;
-		int solved = 0;
 		
-		double hillClimbingNodes = 0;
+		DecimalFormat df = new DecimalFormat("###.##");
+		double counter = 0;
+		double solved = 0;
+		
+		int hillClimbingNodes = 0;
+		int randomRestartNodes = 0;
 		
 		
 		//Hill Climbing Search
@@ -23,11 +28,11 @@ public class Main {
 			}else{
 				counter++;
 			}
-			hillClimbingNodes = (double) search.getNodeCounter();
 		}
-		double hillSolvePercent = (solved/counter) *100;
+		hillClimbingNodes = search.getNodeCounter();
+		double hillSolvePercent = ((solved/counter)*100);
 		hillClimbingNodes = hillClimbingNodes/NUMBER_OF_PUZZLES;
-		System.out.println("Percent solved: " + hillSolvePercent + "%");
+		System.out.println("Percent solved: " + df.format(hillSolvePercent) + "%");
 		System.out.println("Node Average: "+ hillClimbingNodes);
 	
 		search = new Search();
@@ -36,9 +41,9 @@ public class Main {
 		solved = 0;
 		
 		//Random-Restart Hill Climbing Search
-		/*for(int i = 0; i < NUMBER_OF_PUZZLES; i++){
+		for(int i = 0; i < NUMBER_OF_PUZZLES; i++){
 			board.resetBoard();
-			board = search.randomRestart();
+			board = search.randomRestart(board);
 			if(board.getHeuristic() == 0){
 				solved ++;
 				counter++;
@@ -46,9 +51,10 @@ public class Main {
 				counter++;
 			}
 		}
+		randomRestartNodes = search.getNodeCounter();
 		double randomSolvePercent = (solved/counter) *100;
-		double randomClimbingNodes = search.getNodeCounter()/100;
+		randomRestartNodes = randomRestartNodes/NUMBER_OF_PUZZLES;
 		System.out.println("Percent solved: " + randomSolvePercent + "%");
-		System.out.println("Node Average: "+ randomClimbingNodes);
-	*/}
+		System.out.println("Node Average: "+ randomRestartNodes);
+	}
 }
