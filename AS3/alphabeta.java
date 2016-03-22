dd09bea8a67f7f984dd309724e2171c3d4bcade7
K-talebi <K-talebi@users.noreply.github.com>
2016-03-22 10:56:24 -0300

    Create alphabeta.java

---
+++ AS3/alphabeta.java
@@ -0,0 +1,19 @@
+public class alphabeta {
+
+	public static void main (String [] args){
+		GameTree gameTree = new GameTree();
+		int [] treeVal = new int [9];
+		
+		//put args into an array to be made into a gametree
+		for (int i = 0; i < 9; i++){
+			treeVal [i] = Integer.parseInt(args[i]);
+			System.out.println(treeVal[i]);
+		}
+		gameTree.fillTree(treeVal);
+		
+		Search search = new Search();
+		
+		search.aBSearch(gameTree);
+	}
+
+}
