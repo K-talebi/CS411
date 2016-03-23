0bab05cfb362a98d1719460549f51c620b82fb4b
K-talebi <K-talebi@users.noreply.github.com>
2016-03-22 21:13:06 -0300

    Update Search.java

--- AS3/Search.java
+++ AS3/Search.java
@@ -7,77 +7,41 @@ public class Search {
 		
 	}
 	
-	public int aBSearch(GameTree current){
-			return maxValue(current, Integer.MIN_VALUE, Integer.MAX_VALUE);
-	}
-	
-	public int maxValue(GameTree current, int alpha, int beta){
-		//check if leaf node, if it is, check if it is in visited list, if not add it
+	public int aBSearch(GameTree current, int alpha, int beta, boolean isMax){
 		if(terminalTest(current)){
-			if(!(visited.contains(current))){
-				visited.add(current);
-			}
 			return utility(current);
 		}
-		//set newVal as smallest possible number so it will be changed at start 100% of time
-		int newVal = Integer.MIN_VALUE;
-		//loop through each child of current tree and call minValue on it
-		for(GameTree tree : current.getChildList()){
-			
-			if(newVal >= minValue(tree,alpha,beta)){
-				newVal = minValue(tree,alpha,beta);
-			}
-			//if new val is greater than alpha set new alpha
-			if(newVal >= alpha){
-				current.setData(newVal);
-				return newVal;
-			}
-			//if new val is smaller than current beta set new beta
-			if(newVal <= beta){
-				beta = newVal;
-			}
-		}
-			current.setData(newVal);
-			return newVal;	
-	}
-	
-	public int minValue(GameTree current, int alpha, int beta){
-		if(terminalTest(current)){
-			if(!(visited.contains(current))){
-				visited.add(current);
-			}
-			return utility(current);
-		}
-		int newVal = Integer.MAX_VALUE;
-		for(GameTree tree : current.getChildList()){
-			if(newVal >= maxValue(tree,alpha,beta)){
-				newVal = maxValue(tree,alpha,beta);
-			}
-			//if newVal is smaller than alpha set newAlpha
-			if(newVal <= alpha){
-				current.setData(newVal);
-				return newVal;
-			}
-			//if newVal is larger than current beta set newBeta
-			if(newVal >= beta){
-				beta = newVal;
-			}
-		}
-			current.setData(newVal);
-			return newVal;	
-	}
-	
-	public int max(int v, int minVal){
-		return minVal;
 		
-	}
-	
-	public int min(int v, int maxVal){
-		return maxVal;
-		
+		if(isMax){
+			int v = Integer.MIN_VALUE;
+			for(GameTree tree : current.getChildList()){
+				v = Math.max(v, aBSearch(tree, alpha,beta, false));
+				alpha = Math.max(alpha, v);
+				if(beta <= alpha){
+					break;
+				}
+				return v;
+			}
+		}
+		else{
+			int v = Integer.MAX_VALUE;
+			for(GameTree tree : current.getChildList()){
+				v = Math.min(v, aBSearch(tree,alpha,beta, true));
+				beta = Math.min(beta, v);
+				if(beta <= alpha){
+					break;
+				}
+			}
+			return v;
+		}
+		System.out.println("ERROR");
+		return 0;
 	}
 	
 	public boolean terminalTest(GameTree current){
+		if(current.getChild(0) == null){
+			return true;
+		}
 		return !(current.hasChildren());
 		
 	}
