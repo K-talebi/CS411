774dab0d772d488759d8439edc1bff3f47f5c2b4
K-talebi <K-talebi@users.noreply.github.com>
2016-04-04 19:17:40 -0300

    Update Search.java

--- AS4/Search.java
+++ AS4/Search.java
@@ -2,17 +2,33 @@ import java.util.*;
 
 public class Search {
 
-	public boolean arcCheck(ArrayList<Node> nodeList){
-		
+	public boolean arcCheck(Queue<Node> nodeQueue){
+		int i = 0;
+		while(nodeQueue.size() > 0){
+			Node x = nodeQueue.poll();
+			Node y = nodeQueue.poll();
+			if(arcRevise(x, y)){
+				if(x.getDomain().size() == 0){
+					return false;
+				}
+				for(Node k :x.getConnected()){
+					nodeQueue.add(k);
+				}
+			}
+			i++;
+		}
+		return true;
 	}
 
 	public boolean arcRevise(Node x, Node y){
-		boolean change = false;
+		boolean revised = false;
 		for(String color : y.getDomain()){
-			if(color != x.getColor()){
-				
+			if(!x.canSatisfy(color)){
+				y.getDomain().remove(color);
+				revised = true;
 			}
 		}
-		return change;
+		return revised;
 	}
 }
+
