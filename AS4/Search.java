7e3f9c3549293ae115e23dbcb3773076dbb5c282
K-talebi <K-talebi@users.noreply.github.com>
2016-04-03 23:36:18 -0300

    Create Search.java

---
+++ AS4/Search.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Search {
+
+	public boolean arcCheck(ArrayList<Node> nodeList){
+		
+	}
+
+	public boolean arcRevise(Node x, Node y){
+		boolean change = false;
+		for(String color : y.getDomain()){
+			if(color != x.getColor()){
+				
+			}
+		}
+		return change;
+	}
+}
